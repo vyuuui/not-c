22663440cbc4b18a792578bc395b346cf9bb37fa
Crash <gkcrash@hotmail.com>
2022-06-13 20:02:59 -0700

    add strings

--- lexer.hs
+++ lexer.hs
@@ -9,11 +9,13 @@ module Lexer
 import qualified Data.Set as S
 import qualified Data.List as L
 import qualified Data.Char as C
+import System.IO
 
 data ConstantType
   = IntConstant Integer
   | FloatConstant Float
   | BoolConstant Bool
+  | StringConstant String
   deriving Show
 
 data Token
@@ -57,7 +59,8 @@ isPunctuation :: Char -> Bool
 isPunctuation h = S.member [h] punctuation
 
 types :: S.Set String
-types = S.fromList ["void", "char", "short", "int", "long", "float", "double", "bool"]
+types = S.fromList ["void", "char", "short", "int", "long", "float", "double", "bool", "string"]
+
 
 lexStringSingle :: String -> LexerResult
 lexStringSingle (h:t)
@@ -69,8 +72,25 @@ lexStringSingle (h:t)
     | Lexer.isLetter h  = let (token, rest) = span isIdentifier t
                           in (classifyLetterToken (h:token), rest)
     | C.isSpace h       = lexStringSingle $ eatWs t
+    | h == '"'          = lexStringConstant t
     | otherwise         = (Invalid, t)
   where
+    lexStringConstant :: String -> LexerResult
+    lexStringConstant str
+        = tryAppendStr $ until endingQuote buildString ("", str)
+      where
+        endingQuote (_, h1:t) = h1 == '"'
+        endingQuote (_, []) = True
+        buildString (lhs, h1:h2:t)
+            | h1 == '\\' && h2 == 'n' = (lhs ++ ['\n'], t)
+            | h1 == '\\' && h2 == 't' = (lhs ++ ['\t'], t)
+            | h1 == '\\' && h2 == 'r' = (lhs ++ ['\r'], t)
+            | h1 == '\\' && h2 == '"' = (lhs ++ ['"'], t)
+            | otherwise               = (lhs ++ [h1], h2:t)
+        buildString (lhs, [h]) = (lhs ++ [h], t)
+        buildString (lhs, [])  = (lhs, "")
+        tryAppendStr (buildStr, _:t) = (Constant $ StringConstant buildStr, t)
+        tryAppendStr (_, rest) = (Invalid, rest)
     eatWs str = dropWhile C.isSpace str
     classifyNumberToken str
         | '.' `elem` str = Constant $ FloatConstant (read str :: Float)
@@ -82,7 +102,7 @@ lexStringSingle (h:t)
         | S.member str punctuation = Punctuation str
         | S.member str types       = TypeName str
         | otherwise                = Identifier str
-    spanOperator :: [Char] -> (String, String)
+    spanOperator :: String -> (String, String)
     spanOperator str = (extracted, last next:rest)
       where
         checkStop (cur, next, [])    = True
